10b607cd80521052bb862b871edf26dad4d610b4
dakshina <mihirvelaga21144@gmail.com>
2025-06-19 12:28:42 +0530

    Update AdminLogin.jsx

--- client/src/Components/AdminLogin.jsx
+++ client/src/Components/AdminLogin.jsx
@@ -14,7 +14,7 @@ const AdminLogin = () => {
   const navigate = useNavigate();
   const handleSubmit = async () => {
     try {
-      const results = await axios.post("http://localhost:5001/login/admin", {
+      const results = await axios.post("hi/login/admin", {
         username,
         password,
       });
